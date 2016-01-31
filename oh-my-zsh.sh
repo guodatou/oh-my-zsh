779a3823c9707ff463b6e9ae83a1d9bb54046d1e
guodatou <guosongchu@gmail.com>
2016-02-01 00:31:05 +0800

    undo scguo test

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,5 +1,4 @@
 # Check for updates on initial load...
-# scguo git test
 if [ "$DISABLE_AUTO_UPDATE" != "true" ]; then
   env ZSH=$ZSH DISABLE_UPDATE_PROMPT=$DISABLE_UPDATE_PROMPT zsh -f $ZSH/tools/check_for_upgrade.sh
 fi
